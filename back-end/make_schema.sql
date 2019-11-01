072d3be447ec59a3bf887dd05eb6b279d3850443
Daniel Gealow <dgealow2@illinois.edu>
2019-10-31 19:20:26 -0500

    Tidied schema

--- back-end/make_schema.sql
+++ back-end/make_schema.sql
@@ -5,43 +5,42 @@ CREATE TABLE IF NOT EXISTS test(
        name VARCHAR(32)
 );
 
-CREATE DATABASE IF NOT EXISTS googleScholar;
-USE googleScholar;
+--CREATE DATABASE IF NOT EXISTS googleScholar;
+--USE googleScholar;
 
 CREATE TABLE IF NOT EXISTS Author(
-    AuthorId INTEGER PRIMARY KEY, 
-    Name TEXT NOT NULL, 
-    Affiliation TEXT, 
-    CitedBy INTEGER, 
-    HIndex INTEGER, 
+    AuthorId INTEGER PRIMARY KEY,
+    Name TEXT NOT NULL,
+    Affiliation TEXT,
+    CitedBy INTEGER,
+    HIndex INTEGER,
     I10Index INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Article(
     ArticleId INTEGER PRIMARY KEY,
-    PrimaryAuthorId INTEGER, 
-    CitedBy INTEGER, 
-    Citations INTEGER, 
-    Title TEXT NOT NULL, 
-    Year INTEGER, 
-    Url TEXT, 
-    Publisher TEXT, 
-    Journal TEXT, 
+    PrimaryAuthorId INTEGER,
+    CitedBy INTEGER,
+    Citations INTEGER,
+    Title TEXT NOT NULL,
+    Year INTEGER,
+    Url TEXT,
+    Publisher TEXT,
+    Journal TEXT,
     FOREIGN KEY (PrimaryAuthorId) REFERENCES Author(AuthorId)
 );
 
-
 CREATE TABLE IF NOT EXISTS InterestedIn(
-    AuthorId INTEGER, 
-    Interest VARCHAR(255), 
-    PRIMARY KEY (AuthorId, Interest), 
+    AuthorId INTEGER,
+    Interest VARCHAR(255),
+    PRIMARY KEY (AuthorId, Interest),
     FOREIGN KEY (AuthorID) REFERENCES Author(AuthorId)
 );
 
 CREATE TABLE IF NOT EXISTS YearlyCitations(
-    AuthorId INTEGER, 
-    Year INTEGER, 
-    Citations INTEGER NOT NULL, 
+    AuthorId INTEGER,
+    Year INTEGER,
+    Citations INTEGER NOT NULL,
     PRIMARY KEY (AuthorId, Year),
     FOREIGN KEY (AuthorID) REFERENCES Author(AuthorId)
 );
